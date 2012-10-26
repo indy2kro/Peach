bbb59d5d98bd57b57e5a41b25d9d7edce3ea362c
Indiana Jones <Indiana Jones@SuperIndy>
2012-10-26 22:03:33 +0300

    [code] Added registry

--- lib/Peach/Registry.php
+++ lib/Peach/Registry.php
@@ -10,9 +10,118 @@
 /**
  * Registry implementation
  */
-class Peach_Registry
+class Peach_Registry extends ArrayObject
 {
-    // TODO
+    /**
+     * Registry object provides storage for shared objects
+     * 
+     * @var Peach_Registry
+     */
+    protected static $_registry;
+
+    /**
+     * Retrieves the registry instance
+     *
+     * @return Peach_Registry
+     */
+    public static function getInstance()
+    {
+        if (is_null(self::$_registry)) {
+            self::_init();
+        }
+
+        return self::$_registry;
+    }
+
+    /**
+     * Unset the default registry instance.
+     * 
+     * @returns void
+     */
+    public static function unsetInstance()
+    {
+        self::$_registry = null;
+    }
+
+    /**
+     * getter method, basically same as offsetGet().
+     *
+     * This method can be called from an object of type Peach_Registry, or it
+     * can be called statically.  In the latter case, it uses the default
+     * static instance stored in the class.
+     *
+     * @param string $index Index to get
+     * @return mixed
+     * @throws Peach_Exception
+     */
+    public static function get($index)
+    {
+        $instance = self::getInstance();
+
+        if (!$instance->offsetExists($index)) {
+            throw new Peach_Exception("No entry is registered for key '$index'");
+        }
+
+        return $instance->offsetGet($index);
+    }
+
+    /**
+     * setter method, basically same as offsetSet().
+     *
+     * This method can be called from an object of type Peach_Registry, or it
+     * can be called statically.  In the latter case, it uses the default
+     * static instance stored in the class.
+     *
+     * @param string $index The location in which to store the value
+     * @param mixed  $value The value to store
+     * @return void
+     */
+    public static function set($index, $value)
+    {
+        $instance = self::getInstance();
+        $instance->offsetSet($index, $value);
+    }
+
+    /**
+     * Returns TRUE if the $index is a named value in the registry,
+     * or FALSE if $index was not found in the registry.
+     *
+     * @param string $index
+     * @return boolean
+     */
+    public static function isRegistered($index)
+    {
+        if (is_null(self::$_registry)) {
+            return false;
+        }
+        
+        return self::$_registry->offsetExists($index);
+    }
+    
+    /**
+     * Remove a value from the registry
+     *
+     * @param string $index
+     * @return void
+     */
+    public static function remove($index)
+    {
+        if (is_null(self::$_registry)) {
+            return null;
+        }
+        
+        self::$_registry->offsetUnset($index);
+    }
+    
+    /**
+     * Initialize the registry instance
+     *
+     * @return void
+     */
+    protected static function _init()
+    {
+        self::$_registry = new Peach_Registry(array(), self::ARRAY_AS_PROPS);
+    }
 }
 
 /* EOF */
