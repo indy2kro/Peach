0107ae17786f390cdfa217171e500b75c5d7d080
Indiana Jones <Indiana Jones@SuperIndy>
2012-10-26 22:03:53 +0300

    [code] Added version

---
+++ lib/Peach/Version.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Peach Framework
+ *
+ * @category   Peach
+ * @package    Peach_Version
+ * @copyright  Copyright (c) 2012 Peach Library
+ */
+
+/**
+ * Version implementation
+ */
+final class Peach_Version
+{
+    /**
+     * Library version
+     */
+    const VERSION = '0.0.1';
+}
+
+/* EOF */
