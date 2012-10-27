adb157c23267f6ca7f694c902435536876edaf34
Indiana Jones <Indiana Jones@SuperIndy>
2012-10-27 09:59:36 +0300

    [code] Added bootstrap for unit tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Peach Library tests
+ *
+ * @category   PeachTest
+ * @package    PeachTest
+ * @copyright  Copyright (c) 2012 Peach Library
+ */
+
+require_once dirname(__FILE__) . '/PeachTest/Bootstrap.php';
+
+$bootstrap = new PeachTest_Bootstrap();
+$bootstrap->init();
+
+/* EOF */
